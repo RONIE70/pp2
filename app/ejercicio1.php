0b84a7d5a3ce23202a22999866e7a5e7fd34e51e
RONIE70 <graficascorpions.com>
2021-04-18 18:55:41 -0300

    ejercicio1

---
+++ app/ejercicio1.php
@@ -0,0 +1,17 @@
+<?php
+
+//echo rand (1,70);
+
+//$edad-15;
+echo $edad= rand (1,70);
+echo "<br>";
+
+if ($edad<=15)
+{
+	echo $edad. " es menor";
+}
+else {
+	echo $edad. " es mayor";
+}
+
+?>
---
+++ ejercicio01.php
@@ -0,0 +1,17 @@
+<?php
+
+echo rand (1,70);
+
+$edad-15;
+$edad= rand (1,70);
+
+
+if ($edad<=15)
+{
+	echo $edad. " es menor";
+}
+if else
+{
+	echo $edad. " es mayor";
+}
+?>
