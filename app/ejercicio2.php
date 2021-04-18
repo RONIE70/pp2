7435e3b243b04f12ec1ebef6ab62edd66113ed08
RONIE70 <graficascorpions.com>
2021-04-18 20:29:18 -0300

    agrego ejercicio2

---
+++ app/ejercicio2.php
@@ -0,0 +1,20 @@
+<?php
+
+/*echo " El valor de su producto mas IVA es: ";
+
+echo $art= rand (1,500);
+$iva=1.21;
+echo $art*$iva
+
+echo "<br>";*/
+$precioneto = 101.98;
+ $iva = 0.196;
+ $resultado = $precioneto * $iva;
+ echo "El precio es de ";
+ echo $precioneto;
+ echo " y el IVA el ";
+ echo $iva;
+ echo "% <br>";
+ echo "Resultado: " ;
+
+?>
